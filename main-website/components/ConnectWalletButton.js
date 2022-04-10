8cc9ff262e931fc2b7601ec993630329a105c3d0
pshpis <p.shpis@inbox.ru>
2022-04-10 13:56:27 +0300

    feat: add ConnectWalletButton.js component

--- main-website/components/ConnectWalletButton.js
+++ main-website/components/ConnectWalletButton.js
@@ -21,11 +21,16 @@ import {
     ModalBody,
     ModalCloseButton,
 } from '@chakra-ui/react';
-import { Stack, HStack, VStack } from '@chakra-ui/react';
-import { Text } from '@chakra-ui/react'
-import { SimpleGrid } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react';
+import { SimpleGrid } from '@chakra-ui/react';
+import {useState} from "react";
+import { useToast } from '@chakra-ui/react'
 
+function cutAddress(address){
+    return address.slice(0, 5) + "..." + address.slice(address.length - 4);
+}
 
+const PolygonChainId = 137;
 export const ConnectWalletButton = () => {
     const connectWithCoinbaseWallet = useCoinbaseWallet();
     const connectWithMetamask = useMetamask();
@@ -33,24 +38,57 @@ export const ConnectWalletButton = () => {
     const disconnectWallet = useDisconnect();
     const address = useAddress();
     const network = useNetwork();
+
     
     const {isOpen, onOpen, onClose} = useDisclosure();
+    const [isHover, setIsHover] = useState(false);
+
+    const toast = useToast();
 
     if (address) {
+        let modal = "";
+        if (network[0].data.chain.id !== PolygonChainId){
+            modal =
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay/>
+                <ModalContent>
+                    <ModalCloseButton/>
+                    <ModalHeader><Text align="center">Change your network</Text></ModalHeader>
+                    <ModalBody>
+                        <Text>
+                            All our services work on polygon <br/>
+                            Please switch your network to <b>Polygon Mainnet</b>
+                        </Text>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme='blue' onClick={onClose}>
+                            OK
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        }
         return (
             <>
-                Address: {address}
-                <br />
-                Chain ID: {network[0].data.chain && network[0].data.chain.id}
-                <br />
-                <Button onClick={disconnectWallet}>Disconnect</Button>
+                {modal}
+                <Button width="200px" onClick = {event => { disconnectWallet(); onClose();}}
+                        leftIcon={<Icon as={MdOutlineAccountBalanceWallet} w="6" h="6"/>}
+                        colorScheme='red' variant={isHover? 'solid':'outline'}S
+                        onMouseEnter={() => setIsHover(true)}
+                        onMouseLeave={() => setIsHover(false)}>
+                    { isHover ? "Disconnect wallet" : cutAddress(address)}
+                </Button>
             </>
         );
     }
 
-    return(
+    return (
         <>
-            <Button onClick={onOpen} leftIcon={<Icon as={MdOutlineAccountBalanceWallet} w="6" h="6"/>} colorScheme='red' variant='outline'>
+            <Button width="200px" onClick={() => { onOpen(); }}
+                    leftIcon={<Icon as={MdOutlineAccountBalanceWallet} w="6" h="6"/>}
+                    colorScheme='red' variant='outline'
+                    onMouseEnter={() => setIsHover(true)}
+                    onMouseLeave={() => setIsHover(false)}>
                 Connect Wallet
             </Button>
             <Modal isOpen={isOpen} onClose={onClose} isCentered>
@@ -60,7 +98,9 @@ export const ConnectWalletButton = () => {
                     <ModalHeader><Text align="center">Connect your wallet</Text></ModalHeader>
                     <ModalBody>
                         <SimpleGrid column={1} spacing={5}>
-                            <Button onClick={() => connectWithMetamask()}>Connect MetaMask</Button>
+                            <Button onClick={() => connectWithMetamask()}>
+                                Connect MetaMask
+                            </Button>
                             <Button onClick={() => connectWithWalletConnect()}>
                                 Connect WalletConnect
                             </Button>
--- main-website/pages/index.js
+++ main-website/pages/index.js
@@ -2,5 +2,7 @@ import {ConnectWallet} from "../components/ConnectWallet";
 import {ConnectWalletButton} from "../components/ConnectWalletButton";
 
 export default function Home(){
-    return <ConnectWalletButton />
+    return <div style={{"padding": "5px"}}>
+        <ConnectWalletButton />
+    </div>
 }
