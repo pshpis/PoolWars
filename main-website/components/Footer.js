69cd6acece6a452380b73d3ba8d10e361c2623bd
Serge Semenidov <23sergey03@gmail.com>
2022-04-17 14:54:09 +0300

    footer header tempalte

--- main-website/components/Footer.js
+++ main-website/components/Footer.js
@@ -1,5 +1,13 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <div className="footer__logo">PoolWars</div>
+        <div className="footer__socials">socials</div>
+        <div className="footer__info">INFO</div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- main-website/components/Header.js
+++ main-website/components/Header.js
@@ -2,39 +2,41 @@ import React from "react";
 import Link from "next/link";
 function Header() {
   return (
-    <div className="wrapper">
-      <Link href="/">
-        <a className="logo">PoolWars</a>
-      </Link>
-      <div className="panel">
-        <div className="nav">
-          <ul className="nav__list">
-            <li className="nav__item">
-              <Link href="/airdrop">
-                <a className="nav__link">AirDroap</a>
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link href="/#roadMap">
-                <a className="nav__link">RoadMap</a>
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link href="/profile">
-                <a className="nav__link">Profile</a>
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link href="/#faq">
-                <a className="nav__link">FAQ</a>
-              </Link>
-            </li>
-          </ul>
+    <header className="header">
+      <div className="header__container container">
+        <Link href="/">
+          <a className="logo">PoolWars</a>
+        </Link>
+        <div className="panel">
+          <div className="nav">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <Link href="/airdrop">
+                  <a className="nav__link">AirDroap</a>
+                </Link>
+              </li>
+              <li className="nav__item">
+                <Link href="/#roadMap">
+                  <a className="nav__link">RoadMap</a>
+                </Link>
+              </li>
+              <li className="nav__item">
+                <Link href="/profile">
+                  <a className="nav__link">Profile</a>
+                </Link>
+              </li>
+              <li className="nav__item">
+                <Link href="/#faq">
+                  <a className="nav__link">FAQ</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="socials">socials</div>
+          <div className="metamask">metamask</div>
         </div>
-        <div className="socials">socials</div>
-        <div className="metamask">metamask</div>
       </div>
-    </div>
+    </header>
   );
 }
 
