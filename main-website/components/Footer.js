8d2c0ec735003ef3a1152eebc5b73ce64dc4bc49
Serge Semenidov <23sergey03@gmail.com>
2022-04-17 16:49:47 +0300

    footer style

--- main-website/components/Footer.js
+++ main-website/components/Footer.js
@@ -1,10 +1,31 @@
+import React from "react";
+
+import footer from "../styles/footer.module.css";
+import global from "../styles/global.module.css";
+
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer__container container">
-        <div className="footer__logo">PoolWars</div>
-        <div className="footer__socials">socials</div>
-        <div className="footer__info">INFO</div>
+    <footer className={footer.footer}>
+      <div className={`${footer.footer__container} ${global.container}`}>
+        <div className={footer.footer__top}>
+          <div className={footer.footer__logo}>PoolWars</div>
+          <ul className={footer.footer__social}>
+            <li className={footer.footer__social__item}>social 1</li>
+            <li className={footer.footer__social__item}>social 2</li>
+            <li className={footer.footer__social__item}>social 3</li>
+            <li className={footer.footer__social__item}>social 4</li>
+            <li className={footer.footer__social__item}>social 5</li>
+          </ul>
+        </div>
+        <div className={footer.footer__bottom}>
+          <div className={footer.footer__side}></div>
+          <div className={footer.footer__desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+            consectetur ipsum eaque dicta eius quae impedit voluptate, ipsa
+            veritatis. Unde illo excepturi nesciunt quam perspiciatis ad
+            praesentium, atque obcaecati est!
+          </div>
+        </div>
       </div>
     </footer>
   );
