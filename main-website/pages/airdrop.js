c5790d787f77955d4723640d410269c0e280831c
Serge Semenidov <23sergey03@gmail.com>
2022-04-23 13:59:27 +0300

    basic airdrop

--- main-website/pages/airdrop.js
+++ main-website/pages/airdrop.js
@@ -1,10 +1,25 @@
+import { useAddress, useNFTDrop } from "@thirdweb-dev/react";
 import React from "react";
 import PageContainer from "../components/PageContainer";
 
 function Airdrop() {
+  const nftDrop = useNFTDrop("0xF643591DC6b5c3516a0B748500aCe87ea57558aF");
+  const address = useAddress();
+  const MintNft = () => {
+    if (!nftDrop || !address) return;
+    nftDrop
+      .claimTo(address, 1)
+      .then(async (tx) => {
+        console.log(tx);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <PageContainer>
       <div>Airdrop</div>
+      <button onClick={MintNft}>mint</button>
     </PageContainer>
   );
 }
