82e7acc5b0992328cb7c55aa796491d9377dcf81
pshpis <p.shpis@inbox.ru>
2022-05-13 12:40:49 +0300

    fix: try increase loading stats

--- main-website/next.config.js
+++ main-website/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
   reactStrictMode: true,
   async redirects() {
@@ -18,4 +22,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig);
