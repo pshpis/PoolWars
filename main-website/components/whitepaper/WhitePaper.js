51a2b724f283f5ab504f9552da271366f93555d1
pshpis <p.shpis@inbox.ru>
2022-05-18 16:39:23 +0300

    fix: optimize whitepaper

--- main-website/components/whitepaper/WhitePaper.js
+++ main-website/components/whitepaper/WhitePaper.js
@@ -1,21 +1,10 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {getContent, getSectionBySectionPathName} from "./navigation";
 import {Layout} from "./layout/Layout";
 import {NavigationPanel} from "./NavigationPanel";
-import {useRouter} from "next/router";
 
-export const WhitePaper = () => {
-    let [currentSection, setCurrentSection] = useState("");
-
-    const {query, isReady} = useRouter();
-    useEffect(() => {
-        if (isReady){
-            const {section} = query;
-            let sectionName = getSectionBySectionPathName(section);
-            setCurrentSection(sectionName);
-        }
-    }, [isReady]);
-
+export const WhitePaper = ({defaultSection}) => {
+    let [currentSection, setCurrentSection] = useState(defaultSection);
 
     let content = getContent(currentSection);
 
