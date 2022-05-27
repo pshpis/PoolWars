6ee55b506faed969c7d1c8dcc26e6c2179781894
teeleontee <teeleontee@gihub.com>
2022-05-27 23:53:11 +0300

    Fixed Governance.js

--- main-website/components/whitepaper/content-blocks/Governance.js
+++ main-website/components/whitepaper/content-blocks/Governance.js
@@ -3,14 +3,14 @@ import {Heading, Text} from "@chakra-ui/react";
 export const Governance = () => {
     return <>
         <Heading fontSize="48px" lineHeight="54px">Governance</Heading>
-        <Text fontSize="16px" color="rgba(136,153,168,1.00)" marginTop="16px">”Pool Wars” will have their own governance system and governance token in order to have a better connection with our community.</Text>
+        <Text fontSize="16px" color="rgba(136,153,168,1.00)" marginTop="16px">”Pool Wars” will have their own governance system and governance token in order to have  better communication with our community.</Text>
         <hr style={{marginTop: "20px",marginBottom:"20px"}}/>
 
-        <Text padding="8px 0px" fontSize="16px" lineHeight="24px">An <b>exclusive</b> ”Pool Wars” governance token will be issued to all users who play games on our platform. Thanks to <b><i>GameFi</i></b> and <b><i>Play-to-Earn</i></b> concepts, users will be able to earn rewards not only for winning games, but also for participating in them.</Text>
+        <Text padding="8px 0px" fontSize="16px" lineHeight="24px">A <b>exclusive</b> ”Pool Wars” governance token will be issued to all the users who play games on our platform. Thanks to <b><i>GameFi</i></b> and <b><i>Play-to-Earn</i></b> concepts, users will be able to earn rewards not only for winning games, but also for participating in them.</Text>
 
-        <Text padding="8px 0px" fontSize="16px" lineHeight="24px">Everyone will be able to buy a <b>governance token</b> on popular decentralized (<b>DeFi</b>) exchanges.</Text>
+        <Text padding="8px 0px" fontSize="16px" lineHeight="24px">Everyone will be able to buy a <b>governance token</b> using popular decentralized (<b>DeFi</b>) exchanges.</Text>
 
-        <Text padding="8px 0px" fontSize="16px" lineHeight="24px">Also, users with a token will be able to participate in the <b>governance system</b>, vote for project decisions, such as new <b>partnerships</b>, new <b>features</b>, <b>changes</b>, and so on.
+        <Text padding="8px 0px" fontSize="16px" lineHeight="24px">Users with a token will be able to participate in the <b>governance system</b>. These users will be able to vote for project decisions, like new <b>partnerships</b>, new <b>features</b>, <b>changes</b>, and more.
         </Text>
 
     </>;
