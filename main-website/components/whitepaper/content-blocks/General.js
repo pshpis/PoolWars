29f56b37836de7b1f2c6111f35fd365ab25fe880
pshpis <51034972+pshpis@users.noreply.github.com>
2022-06-18 17:34:54 +0300

    fix: general text

--- main-website/components/whitepaper/content-blocks/General.js
+++ main-website/components/whitepaper/content-blocks/General.js
@@ -1,5 +1,5 @@
 import {Heading, Text} from "@chakra-ui/react";
-
+import Link from "next/link";
 export const General = () => {
     return <>
         <Heading fontSize="48px" lineHeight="54px">General</Heading>
@@ -7,7 +7,7 @@ export const General = () => {
         <hr style={{marginTop: "20px",marginBottom:"20px"}}/>
 
         <Text fontWeight="700" fontSize="16px">
-            “Pool Wars” team released an exclusive Play-To-Earn NFT collection built on Polygon, including 20,000 Warlords which are the main part of the &quot;Battle of Conquerors&quot; Metaverse. Warlords NFT will soon be available for minting from our “Pool Wars” website.
+            “Pool Wars” team released an exclusive NFT collection built on Solana, including 10,000 Warlords. Warlords NFT will soon be available for minting from our <Link href="/">“Pool Wars” website.</Link>
         </Text>
         <hr style={{marginTop: "20px", marginBottom: "20px"}}/>
 
