399007401cd78fca25b6a9919ec9564671b88bc8
pshpis <petr.shpis@latoken.com>
2022-07-22 17:28:53 +0300

    fix: adaptive for 4k

--- main-website/components/landing/MainPage/MainPage.js
+++ main-website/components/landing/MainPage/MainPage.js
@@ -1,4 +1,4 @@
-import {Center, Stack} from "@chakra-ui/react";
+import {Box, Center, Flex, Stack} from "@chakra-ui/react";
 import {Welcome} from "./Welcome";
 import {TakeNow} from "./TakeNow";
 import {useWindowSize} from "../../../hooks/useWindowSize";
@@ -13,11 +13,16 @@ export const MainPage = () => {
     let defaultSidePadding = "20px";
     if (size.width < 500) defaultSidePadding = "10px"
     return <Layout>
-        <Stack pt={size.width > 1000? "80px" : "20px"} paddingLeft={size.width > 1300 ? "5.5%" : defaultSidePadding}
-               direction={size.width > 1100 ? "row" : "column"} paddingRight={size.width > 1300 ? "5.5%" : defaultSidePadding}>
-            <Welcome/>
-            <Center w="100%"><TakeNow /></Center>
-        </Stack>
+        <Box  pt={size.width > 1000? "80px" : "20px"} paddingRight={size.width > 1300 ? "6.6%" : defaultSidePadding}
+              paddingLeft={size.width > 1300 ? "6.6%" : defaultSidePadding} w="100%">
+            <Stack direction={size.width > 1100 ? "row" : "column"} maxW="1248px" w="100%" margin="0 auto" >
+                <Welcome/>
+                <Flex w="100%" flexDirection="row-reverse" justifyContent={size.width > 1100 ? "initial":"space-around"} alignItems="center">
+                    <TakeNow />
+                </Flex>
+            </Stack>
+        </Box>
+
         <PreviewSwiper margin="100px 0"/>
         <Roadmap margin="60px 0 80px"/>
         <Team marginBottom="160px"/>
