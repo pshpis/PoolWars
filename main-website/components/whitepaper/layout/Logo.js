c1e8f7e458386fb1d3c05a0fec985b4666481a4f
pshpis <petr.shpis@latoken.com>
2022-07-22 13:41:35 +0300

    fix: whitepaper Header.js

--- main-website/components/whitepaper/layout/Logo.js
+++ main-website/components/whitepaper/layout/Logo.js
@@ -1,20 +1,14 @@
-import {Center, HStack, Text} from "@chakra-ui/react";
+import {Box, Center, HStack, Link, Text} from "@chakra-ui/react";
 import Image from "next/image";
 import logoPic from "../../../public/logo.svg";
 import {useWindowSize} from "../../../hooks/useWindowSize";
 import {useMemo} from "react";
 
 const BorderRight = () => {
-    let style = {
-        "borderRight": "1px solid rgba(211,220,228,1.00)",
-        "position": "absolute",
-        "top": "25%",
-        "right": "0",
-        "height": "50%",
-        "bottom": "0",
-    };
-    return <div style={style}>
-    </div>;
+    return <Box borderRight="1px solid rgba(211,220,228,1.00)" position="absolute" top="25%" right="0"
+        height="50%" bottom="0">
+
+    </Box>;
 }
 
 export const Logo = () => {
@@ -28,7 +22,7 @@ export const Logo = () => {
 
     return (
       <Center marginLeft={marginLeft} width={size.width > 800 ? "100%" : "229px"} height="100%" position="relative">
-          <HStack spacing="16px">
+          <HStack spacing="16px" as={Link} href="/">
               <Image alt="Logo" style={{marginRight:"16px"}} src={logoPic} height={size.width > 800 ? "40px" : "30px"} width={size.width > 800 ? "40px" : "30px"} />
               <Text color="rgb(59, 69, 78)" fontSize="1.15em"><b>The Elder Katts</b></Text>
           </HStack>
