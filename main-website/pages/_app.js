a270c77e09a940a8ca35a12bbd45dd932db68150
kosne1 <akolia2017@list.ru>
2022-07-26 17:44:34 +0300

    fix: change scripts

--- main-website/pages/_app.js
+++ main-website/pages/_app.js
@@ -53,24 +53,6 @@ function MyApp({ Component, pageProps }) {
     }, [router.events])
 
     let result = <>
-        <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <Script
-            id="gtag-init"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-                __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-        />
         <NextNProgress
             color="#B8C3E6"
             startPosition={0.3}
@@ -78,6 +60,18 @@ function MyApp({ Component, pageProps }) {
             height={3}
             showOnShallow={true}
         />
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+        <Script id="google-analytics" strategy="lazyOnload">
+            {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+        </Script>
         {/*<Loader loading={loading}/>*/}
         <Component {...pageProps} />
     </>;
