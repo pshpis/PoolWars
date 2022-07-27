8ea4ca85bb81df498e871e74a0e11064f683950b
kosne1 <akolia2017@list.ru>
2022-07-27 11:29:57 +0300

    fix: change gtag

--- main-website/pages/_app.js
+++ main-website/pages/_app.js
@@ -60,14 +60,14 @@ function MyApp({ Component, pageProps }) {
             height={3}
             showOnShallow={true}
         />
-        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-88WW5S734R`} />
 
         <Script id="google-analytics" strategy="lazyOnload">
             {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    gtag('config', 'G-88WW5S734R', {
                     page_path: window.location.pathname,
                     });
                 `}
