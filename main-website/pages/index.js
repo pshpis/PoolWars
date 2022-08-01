c032a315caf950dec902ae28e826d5b0399ba26d
kosne1 <akolia2017@list.ru>
2022-08-01 13:01:50 +0300

    feat: add cors req

--- main-website/pages/index.js
+++ main-website/pages/index.js
@@ -2,6 +2,20 @@ import {MainPage} from "../components/landing/MainPage/MainPage";
 import {LandingStyles} from "../styles/SectionsGlobalStyles";
 
 export default function Home () {
+    var express = require('express')
+    var cors = require('cors')
+    var app = express()
+
+    app.use(cors())
+
+    app.get('/products/:id', function (req, res, next) {
+        res.json({msg: 'This is CORS-enabled for all origins!'})
+    })
+
+    app.listen(80, function () {
+        console.log('CORS-enabled web server listening on port 80')
+    })
+
     return <>
         {LandingStyles}
         <MainPage/>
