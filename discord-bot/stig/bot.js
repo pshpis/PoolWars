9351e165c3692ccd910270551ecb8ab0a4552571
anton <antkumachev@gmail.com>
2022-08-03 00:28:55 +0300

    Contest snapshot

--- discord-bot/stig/bot.js
+++ discord-bot/stig/bot.js
@@ -41,6 +41,69 @@ bot.on('messageCreate', async message => {
     }
 });
 
+const snapshot = [
+    {
+        "member": "513359691963826196",
+        "invites": 108
+    },
+    {
+        "member": "864115053380894730",
+        "invites": 67
+    },
+    {
+        "member": "402003223734648834",
+        "invites": 61
+    },
+    {
+        "member": "510421124807655424",
+        "invites": 61
+    },
+    {
+        "member": "679845273811681326",
+        "invites": 61
+    },
+    {
+        "member": "548978093444825126",
+        "invites": 30
+    },
+    {
+        "member": "908239225479102525",
+        "invites": 26
+    },
+    {
+        "member": "130219759257124864",
+        "invites": 26
+    },
+    {
+        "member": "786596317475373056",
+        "invites": 25
+    },
+    {
+        "member": "881102902528212992",
+        "invites": 23
+    },
+    {
+        "member": "808397846633119774",
+        "invites": 22
+    },
+    {
+        "member": "838434696848212019",
+        "invites": 19
+    },
+    {
+        "member": "509787732638433298",
+        "invites": 18
+    },
+    {
+        "member": "910937776344666112",
+        "invites": 17
+    },
+    {
+        "member": "836178480244588584",
+        "invites": 17
+    }
+]
+
 bot.on("interactionCreate", async interaction => {
 
     if (interaction.isMessageComponent()) {
@@ -56,9 +119,11 @@ bot.on("interactionCreate", async interaction => {
                 let inviteData = group.find(o => o.member === invite.inviterId);
 
                 if (!inviteData) {
+                    const snapshots = snapshot.find(s => s.member === invite.inviterId)?.invites
+
                     inviteData = {
                         member: invite.inviterId,
-                        invites: 0
+                        invites: !!snapshots ? -snapshots : 0
                     }
 
                     group.push(inviteData)
@@ -68,9 +133,10 @@ bot.on("interactionCreate", async interaction => {
                 return group;
             }, []);
 
-            const leaders = groupedInvites.sort((a, b) => b.invites - a.invites).slice(0, 3);
+            const leaders = groupedInvites.sort((a, b) => b.invites - a.invites);
+
             const leadersInfos = await Promise.all(
-                leaders.map(async l => {
+                leaders.slice(0, 3).map(async l => {
                     return {
                         member: await interaction.guild.members.fetch(l.member),
                         invites: l.invites
@@ -80,10 +146,11 @@ bot.on("interactionCreate", async interaction => {
 
             const uses = invites
                 .filter(i => i.inviter.id === interaction.member.id)
-                .reduce((sum, i) => sum += i.uses, 0);
+                .reduce((sum, i) => sum += i.uses, 0) -
+                snapshot.find(s => s.member === interaction.member.id)?.invites ?? 0;
 
             await interaction.reply({
-                content: `${uses} people invited\n\n**LEADERS**\n` +
+                content: `${uses > 0 ? uses : 0} people invited\n\n**LEADERS**\n` +
                     `1. <@${leadersInfos[0].member.id}> - ${leadersInfos[0].invites}\n` +
                     `2. <@${leadersInfos[1].member.id}> - ${leadersInfos[1].invites}\n` +
                     `3. <@${leadersInfos[2].member.id}> - ${leadersInfos[2].invites}`,
