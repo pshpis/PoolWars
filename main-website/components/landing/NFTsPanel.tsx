2db86da01461e07d8f0f7db6cf51b8c7ad413bd9
kosne1 <akolia2017@list.ru>
2022-08-04 13:00:17 +0300

    style: _placeholder

--- main-website/components/landing/NFTsPanel.tsx
+++ main-website/components/landing/NFTsPanel.tsx
@@ -36,10 +36,8 @@ const NFT = ({src, maxValue, setChooseArr}) => {
                     <Text w="150px" color="#949494">You choose:</Text>
                     <Input p="0" mr="auto" w="30px" type="text"
                            placeholder="0" fontWeight="600" fontSize="24px" lineHeight="36px" color="#71CFC3"
-                           textAlign="right" border="0px" _placeholder={{
-                        color: 'inherit',
-                    }}
-                           onInput={ (evt) => {
+                           textAlign="right" border="0px" _placeholder={{color: 'inherit'}}
+                           onChange={ (evt) => {
                                // @ts-ignore
                                const nftChosen = +evt.target.value;
                                if ((nftChosen ^ 0) !== nftChosen) {
@@ -63,7 +61,6 @@ const NFT = ({src, maxValue, setChooseArr}) => {
                                        position: 'top',
                                        isClosable: true,
                                    });
-
                                }
                                else setChooseArr(NFTsName, nftChosen);}
                            }/>
