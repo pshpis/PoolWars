c132d66286625a4db96067e3b4f26d0a0fdbb6c2
anton <antkumachev@gmail.com>
2022-08-05 11:46:42 +0300

    Some more invites

--- discord-bot/stig/bot.js
+++ discord-bot/stig/bot.js
@@ -139,7 +139,7 @@ bot.on("interactionCreate", async interaction => {
 
 	    groupedInvites.push({
 		    "member": "433633971562217485",
-		    "invites": 545
+		    "invites": 614
 	    });
 
             const leaders = groupedInvites.sort((a, b) => b.invites - a.invites);
