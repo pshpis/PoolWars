285803afb9ea9bbd015c08ad25c57554c61a75df
kosne1 <akolia2017@list.ru>
2022-08-07 13:37:44 +0300

    feat: cursor pointer to plus

--- main-website/components/landing/MainPage/PreviewSwiper.tsx
+++ main-website/components/landing/MainPage/PreviewSwiper.tsx
@@ -49,7 +49,7 @@ function PreviewCard({data, index}: {data: CardData, index: number}){
                 backgroundColor="#E8E8E8"  padding="0 15px 0 24px">
             <Box fontWeight="600" fontSize={size.width > 500? "24px": "20px"} color="#949494">Combat card #{index+1}</Box>
             <Spacer/>
-            <Img src="/plus.svg" onClick={switchInfo} transition="all 0.5s"
+            <Img src="/plus.svg" onClick={switchInfo} cursor="pointer" transition="all 0.5s"
                  transform={isInfoOpen? "rotate(45deg)" : "rotate(0)"}/>
         </HStack>
     </Box>;
