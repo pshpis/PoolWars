a3ba37355a17ae1994416c8f62348c649573543a
Anton <antkumachev@gmail.com>
2022-08-09 20:13:21 +0300

    Database migration

---
+++ pool-wars/pool-wars-v0/PoolWarsV0.Data/Migrations/20220809170954_CardTakeData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PoolWarsV0.Data.Migrations
+{
+    public partial class CardTakeData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PoolAddress",
+                table: "UserEvents",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TakenCards",
+                table: "UserEvents",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PoolAddress",
+                table: "UserEvents");
+
+            migrationBuilder.DropColumn(
+                name: "TakenCards",
+                table: "UserEvents");
+        }
+    }
+}
