fdc680f759f700dd9db4da9a7969da8d17517e7e
Anton <antkumachev@gmail.com>
2022-08-10 03:17:20 +0300

    Remaining files

---
+++ discord-bot/betatest-bot/BetaTestBot/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BetaTestBot;
+using BetaTestBot.Interactions;
+using Discord;
+using Discord.WebSocket;
+
+DiscordSocketClient client = DiscordSingletonClient.GetClient();
+
+client.Log += async message =>
+{
+    await Console.Out.WriteLineAsync(message.Message);
+};
+
+client.Ready += async () =>
+{
+    foreach (var cmd in SlashCommand.Commands)
+    {
+        SlashCommandProperties command = new SlashCommandBuilder()
+            .WithName(cmd)
+            .Build();
+        
+        await client.CreateGlobalApplicationCommandAsync(command);
+    }
+};
+
+client.SlashCommandExecuted += async command =>
+{
+    await SlashCommand.Execute(command);
+};
+
+client.ButtonExecuted += async button =>
+{
+    await ButtonPress.Execute(button);
+};
+
+await client.StartAsync();
+await Task.Delay(-1);
