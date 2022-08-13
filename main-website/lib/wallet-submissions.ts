934539d98908fcca9fcacf1c1d665b88f822beff
Anton <antkumachev@gmail.com>
2022-08-13 14:37:20 +0300

    Fix request

--- main-website/lib/wallet-submissions.ts
+++ main-website/lib/wallet-submissions.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 import {MINT_API_URL, UserStageInfo} from "./mint-instructions";
 
-export async function submitWallet(wallet: string): Promise<void> {
+export async function submitWallet(wallet: string): Promise<boolean> {
 
     try {
-        const response = await axios.post<void>(`${MINT_API_URL}/api/v1/submissions/submit=${wallet}`)
+        const response = await axios.post<void>(`${MINT_API_URL}/api/v1/submissions/submit?wallet=${wallet}`)
+        return true;
     }
     catch (e) {
+        return false;
     }
 }
