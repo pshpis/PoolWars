3a88236f52074a0938982b0a0f9fa60aa2fde5fe
Anton <antkumachev@gmail.com>
2022-08-14 01:10:40 +0300

    Wait for poolwar end

--- pool-wars/pool-wars-v0/PoolWarStarter/Program.cs
+++ pool-wars/pool-wars-v0/PoolWarStarter/Program.cs
@@ -1,20 +1,15 @@
-﻿using System.Net.Http.Json;
-using PoolWarsV0.Pools.Models;
-
-using HttpClient client = new();
+﻿using HttpClient client = new();
 client.DefaultRequestHeaders.TryAddWithoutValidation("POOLTOKEN", "+4L_cmerxMqA");
 
-for (var i = 1; i < 1000; i++)
-{
-    PoolWarDto poolWar = new()
-    {
-        Description = $"Pool War #{i}",
-        End = (long) Math.Floor((DateTime.UtcNow.AddHours(3) - DateTime.UnixEpoch).TotalSeconds)
-    };
+TimeSpan delay = new DateTime(year: 2022, month: 8, day: 14, hour: 4, minute: 0, second: 5, DateTimeKind.Utc) - DateTime.UtcNow;
 
-    HttpResponseMessage response = await client.PostAsJsonAsync("https://elderkatts.com/api/v1/wars/create", poolWar);
+Console.WriteLine("Waiting...");
+await Task.Delay(delay);
 
-    if (response.IsSuccessStatusCode)
-    {
-    }
-}
+HttpResponseMessage response = await client.PostAsync(
+    "https://elderkatts.com/api/v1/winnerData/generate?pools=21wS5GhECzRwSuGXnjWqufy8DTpTnd5emejsnL1Q9y2H&pools=8FohkAFVHobeGdU2oBzJ5hD1dae3K3K53uGEoyujZ3K",
+    content: null);
+
+var body = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(body);
